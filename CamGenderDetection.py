9a80c6c1adf0e230245fe433e5839ef6a679b301
Stéphane Godin <34553036+crocsg@users.noreply.github.com>
2019-04-17 12:27:39 +0200

    update

--- CamGenderDetection.py
+++ CamGenderDetection.py
@@ -1,6 +1,7 @@
 import cv2
 import dlib
 import numpy as np
+import pafy
 
 import GenderPredictor as gc
 
@@ -21,45 +22,57 @@ predictor = dlib.shape_predictor(face_predictor_path)
 #init gender detection
 gpredictor.build_model()
 
-#start video capture
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+#start video streaming
+#url = 'https://youtu.be/9cmWFdBYM0M'
+#url= 'https://youtu.be/vGe1Eb_dyh4'
+#vPafy = pafy.new(url)
+#print (vPafy.streams)
+#play = vPafy.getbest(preftype="mp4")
+#play = vPafy.streams[1]
+#print (play)
 
-cat_name = ("male", "female")
+#start video capture
+#cap = cv2.VideoCapture(play.url)
+cap = cv2.VideoCapture(0)
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+cat_name = ("M", "F")
 
 while(True):
     #capture video image
     ret, frame = cap.read()
 
     dets = detector(frame, 0)
-
+    capture = 0
     if len(dets) > 0:
-        faces = dlib.full_object_detections()
+        #
 
         for detection in dets:
+            color = (0,0,255)
+            faces = dlib.full_object_detections()
             faces.append(predictor(frame, detection))
+            images = dlib.get_face_chips(frame, faces, size=64, padding=0.40)  # extract faces images
 
-        images = dlib.get_face_chips(frame, faces, size=64, padding=0.40)   # extract faces images
+            rimg = cv2.cvtColor(images[0], cv2.COLOR_BGR2RGB)  # roll to rgb. the model need rgb images
 
-        for img in images:
-            rimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # roll to rgb. the model need rgb images
+            cv2.imshow('face' + str(capture), images[0])  # display face image
 
-            cv2.imshow('face', img) # display face image
-
-            prediction = gpredictor.predict (rimg)
+            capture += 1
+            prediction = gpredictor.predict(rimg)
             if (len(prediction) > 0):
-                #print (prediction)
+                # print (prediction)
                 c = np.argmax(prediction)
-                title = cat_name[c] + " " + str(prediction[0])
+                title = cat_name[c]
+                color = (255, 0, 0) if c == 0 else (128, 0, 255)
 
-                if abs(prediction[0][0] - prediction[0][1]) > 0.5:
-                    cv2.putText(frame, title, (10,40), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,0))
-
-        for rec in dets:
-            cv2.rectangle(frame, (rec.tl_corner().x, rec.tl_corner().y),
-                          (rec.br_corner().x, rec.br_corner().y), (255, 0, 0))
-
+                if abs(prediction[0][0] - prediction[0][1]) < 0.5:
+                    color = (0, 255, 0)
+                else:
+                    cv2.putText(frame, title, (detection.tl_corner().x, detection.tl_corner().y),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+                cv2.rectangle(frame, (detection.tl_corner().x, detection.tl_corner().y),
+                              (detection.br_corner().x, detection.br_corner().y), color)
 
     cv2.imshow('frame', frame)
 
